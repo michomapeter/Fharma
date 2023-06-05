1da039b195886b5614d9e104474f8005c5f7cc26
Peter michoma <pmichoma@kabarak.ac.ke>
2023-06-05 10:45:43 -0400

    initialization of the backend service

---
+++ Backend/App.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const port = 3000
+
+const server = http.createServer(function(req,res){
+    res.write('Hello Backend')
+    res.end()
+
+})
+
+server.listen(port, function(error){
+    if(error){
+        console.log('something went wrong',error)
+     }
+      else{
+            console.log('server is listining on port' + port)
+        }
+    }
+)
